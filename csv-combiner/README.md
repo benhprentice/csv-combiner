8a8a6be5f82c8ac8493d7495ba5b738a8a8ab29b
benhprentice <90800451+benhprentice@users.noreply.github.com>
2023-01-30 14:37:18 -0600

    Add files via upload

---
+++ csv-combiner/README.md
@@ -0,0 +1,4 @@
+# CSV Combiner
+
+To run the program in the command line type:
+python3 csv-combiner.py \<arg1\> \<arg2\> \<arg3\> \> *outputFileName*
---
+++ csv-combiner/csv-combiner.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+files = []
+
+for arg in range(1, len(sys.argv)):
+    df = pd.read_csv(sys.argv[arg])
+    filename = sys.argv[arg].split('/')
+    filename = filename[-1]
+    df['filename'] = filename
+    files.append(df)
+
+combined_csv = pd.concat(f for f in files)
+combined_csv.to_csv(sys.stdout, index=False)
